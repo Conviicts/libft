916ece38eec112dbb45127d6ecd0e06adecebd98
jode-vri <jode-vri@student.42.fr>
2023-11-21 23:58:06 +0100

    .

--- ft_parseargs.c
+++ ft_parseargs.c
@@ -6,18 +6,18 @@
 /*   By: jode-vri <jode-vri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 23:18:54 by jode-vri          #+#    #+#             */
-/*   Updated: 2023/11/21 23:46:41 by jode-vri         ###   ########.fr       */
+/*   Updated: 2023/11/21 23:58:03 by jode-vri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 bool	ft_parseargs(t_argsArray args, char *option) {
-	for (size_t i = 0; i < args.len; i++) {
-		if (ft_strncmp(option, args.mappings.option, ft_strlen(option)) == 0) {
-			args.mappings.function();
-			return (true);
-		}
-	}
-	return (false);
+    for (size_t i = 0; i < args.len; i++) {
+        if (ft_strncmp(option, args.mappings[i].option, strlen(option)) == 0) {
+            args.mappings[i].function();
+            return true;
+        }
+    }
+    return false;
 }
